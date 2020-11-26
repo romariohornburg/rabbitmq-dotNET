c47485c9c2da0525935bf5fb437a1d3632a83722
root <root@svp03300k8adm01.cecred.coop.br>
2020-11-26 11:33:06 -0300

    Upload files

---
+++ Receive/Receive.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQDemo.Consumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "Repasse.RabbitMQ",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (sender, eventArgs) =>
+                {
+                    byte[] body = eventArgs.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    //* var message = Encoding.UTF8.GetString(eventArgs.Body);
+
+                    Console.WriteLine(Environment.NewLine + "[New message received] " + message);
+                };
+
+                channel.BasicConsume(queue: "Repasse.RabbitMQ",
+                     autoAck: true,
+                     consumer: consumer);
+
+                Console.WriteLine("Aguardando mensagens para processar");
+                Console.WriteLine("Enter para sair");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQDemo.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+            };
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                while (true)
+                {
+                    Console.WriteLine("Digite o conteudo da mensagem");
+
+                    var teste = Console.ReadLine();
+
+                    channel.QueueDeclare(
+                        queue: "Repasse.RabbitMQ",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string message =
+                        $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - " +
+                        $"Message content: {teste}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "Repasse.RabbitMQ",
+                                         basicProperties: null,
+                                         body: body);
+                }
+            }
+        }
+    }
+}
